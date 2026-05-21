2b0e9584ef8ce601a01a6b1484f5d467af52f2f0
B0nni1b3l <isabelefedaltoaranha@gmail.com>
2026-05-21 13:41:35 -0300

    Mudanca dia 21

--- src/view/ClienteView.java
+++ src/view/ClienteView.java
@@ -1,5 +1,10 @@
 package view;
 
+import model.Cliente;
+import model.ClientePJ;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ClienteView {
@@ -19,5 +24,69 @@ public class ClienteView {
         System.out.println("0 - Voltar");
         System.out.print("Escolha uma opção: ");
     }
-
+    public String lerNome(){
+        System.out.println("Digite o nome da pessoa: ");
+        return sc.nextLine();
+    }
+    public String lerCpf(){
+        System.out.println("Digite o CPF: ");
+        return sc.nextLine();
+    }
+    public String lerTelefone(){
+        System.out.println("Digite o número de telefone: ");
+        return sc.nextLine();
+    }
+    public String lerEmail(){
+        System.out.println("Digite o email: ");
+        return sc.nextLine();
+    }
+    public int lerTipo(){
+        System.out.println("Escolha o tipo:\n[1]PF\n[2]PJ");
+        int tipo = sc.nextInt();
+        sc.nextLine();
+        return tipo;
+    }
+    public String lerCnpj(){
+        System.out.println("Digite o CNPJ: ");
+        return sc.nextLine();
+    }
+    public String lerNomeEmpresa(){
+        System.out.println("Digite o nome da Empresa ");
+        return sc.nextLine();
+    }
+    public void mostrarPF(Cliente c){
+        System.out.println("Nome: " + c.getNome() + "\nCPF: " + c.getCpf() + "\nTelefone: " + c.getTelefone() + "\nEmail: " + c.getEmail() + "\n----------------------");
+    }
+    public void mostrarPJ(ClientePJ c){
+        System.out.println("Nome: " + c.getNome() + "\nCPF: " + c.getCpf() + "\nTelefone: " + c.getTelefone() + "\nEmail: " + c.getEmail() + "\nCNPJ: " + c.getCnpj() + "\nNome da empresa: " + c.getNomeEmpresa());
+    }
+    public void listarPF(ArrayList<Cliente> pfs){
+        System.out.println("Lista de PF:");
+        if(pfs.isEmpty()){
+            for(Cliente c: pfs){
+                mostrarPF(c);
+            }
+        }
+    }
+    public void listarPJ(ArrayList<ClientePJ> pjs){
+        System.out.println("Lista de PJ:");
+        if(!pjs.isEmpty()){
+            for(ClientePJ c: pjs){
+                mostrarPJ(c);
+            }
+        }
+    }
+    public void listarTodos(ArrayList<Cliente> pfs, ArrayList<ClientePJ> pjs){
+        System.out.println("Lista de todos os clientes: ");
+        if(!pfs.isEmpty()){
+            for(Cliente c: pfs){
+                mostrarPF(c);
+            }
+        }
+        if(!pjs.isEmpty()){
+            for(ClientePJ c: pjs){
+                mostrarPJ(c);
+            }
+        }
+    }
 }
